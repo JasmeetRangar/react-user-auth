f39ec8a9900800bdff03cfecc9619cbb650f667e
Jasmeet Rangar <jasmeetsinghr@gmail.com>
2021-05-12 16:17:37 -0400

    Add login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { Router, Route, Switch } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Success from "./components/Success";
+export default function App() {
+	return (
+		<div className="login">
+			<form>
+				<label>
+					<p>Username</p>
+					<input type="text" />
+				</label>
+				<label>
+					<p>Password</p>
+					<input type="password" />
+				</label>
+				<div>
+					<button type="submit">Submit</button>
+				</div>
+			</form>
+		</div>
+	);
 }
-
-export default App;
